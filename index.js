334a2212910ba4cc84678206e01d921e92e05d00
farrelabdillah <farrel.205027@mhs.its.ac.id>
2022-04-19 12:16:08 +0700

    repair

--- index.js
+++ index.js
@@ -72,4 +72,6 @@ app.delete('/api/customers/:id', (req, res) => {
     res.send(customer);
 })
 
-app.listen(3000, () => console.log('Listening to port 3000'))
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
